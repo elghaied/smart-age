410c9958fbad5ad3ede89bd0eb844fd34dca280b
elghaied <elghaied.eslam@gmail.com>
2025-09-21 14:25:19 +0200

    adding docker config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
@@ -35,4 +36,3 @@ const nextConfig = {
   redirects,
 }
 export default withNextIntl(withPayload(nextConfig, { devBundleServerPackages: false }))
-
