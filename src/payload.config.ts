07884ce22300f127de9a7f03328c3a28075137ca
elghaied <elghaied.eslam@gmail.com>
2025-09-22 18:11:08 +0200

    back to vercel

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -118,7 +118,6 @@ export default buildConfig({
       host: process.env.SMTP_HOST,
       port: 587,
       secure: false,
-
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
