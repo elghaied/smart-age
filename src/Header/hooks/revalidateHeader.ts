2ea5b95cf8e0f57642565908dc0daa37662be50f
elghaied <elghaied.eslam@gmail.com>
2026-01-28 10:08:49 +0100

    revalidate header

--- src/Header/hooks/revalidateHeader.ts
+++ src/Header/hooks/revalidateHeader.ts
@@ -1,12 +1,17 @@
 import type { GlobalAfterChangeHook } from 'payload'
 
 import { revalidateTag } from 'next/cache'
+import { locales } from '@/i18n/localization'
 
 export const revalidateHeader: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
   if (!context.disableRevalidate) {
     payload.logger.info(`Revalidating header`)
 
-    revalidateTag('global_header')
+    // Revalidate cache for all locales
+    for (const locale of locales) {
+      revalidateTag(`global_header_${locale}`)
+    }
+    revalidateTag('global_header_default')
   }
 
   return doc
